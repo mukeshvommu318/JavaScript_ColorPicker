17721c632a7af0072b8b94b0c528519a61a5d03e
Mukesh Vommu <mukesh.vommu@gmail.com>
2025-05-24 00:27:27 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,22 @@
+let background = document.getElementById("container")
+let textcolor = document.getElementById("colorText")
+
+function button1(){
+    background.style.backgroundColor="#e0e0e0"
+    textcolor.textContent ="#e0e0e0"
+}
+
+function button2(){
+    background.style.backgroundColor="#6fcf97"
+     textcolor.textContent ="#6fcf97"
+}
+
+function button3(){
+    background.style.backgroundColor="#56ccf2"
+     textcolor.textContent ="#56ccf2"
+}
+
+function button4(){
+    background.style.backgroundColor="#bb6bd9"
+     textcolor.textContent ="#bb6bd9"
+}
